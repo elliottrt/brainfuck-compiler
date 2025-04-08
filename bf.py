3f5de18515a9f63f1af08dfdedf0ba2293854dac
elliottrt <reedelliott1@icloud.com>
2025-04-08 10:03:54 -0400

    fixed flake8 errors

--- bf.py
+++ bf.py
@@ -6,115 +6,119 @@ import sys
 import os
 
 '''
+TODO: use argparse
 TODO: only change rdi/x1 when we switch from reading to writing
 TODO (BUG): ,.,. only reads and writes once -> why?
 TODO: figure out if i386 works
 TODO: x86 compilation target
 TODO (BUG): whey does e.bf cause a bus error?
-TODO: refactor bf_X to return str instead of List[str] -> '...'\n'...' is a single string
 TODO: make options['asm_comments'] a command line option
 TODO: calls to c std library instead of syscalls (option?)
 TODO: add simulation mode
+TODO: add tests
 '''
 
 
 def usage(filename: str) -> None:
-	print()
-	print(f'Usage: {os.path.basename(filename)} <input file> [options]')
-	print('\nOptions:')
-	print('-o <filepath> (output filepath)')
-	print('-no (don\'t optimize output)')
-	print('-nrwf (disable subroutines for read and write)')
-	print('-cc (set number of cells)')
-	print('-cs (cell size in bits - 8, 16, 32, 64)')
-	print('-arch (architecture - i386, x86_64, arm64)')
-	print('-asm (only output assembly file, will use -o <filename> if specified)')
-	print()
+    print()
+    print(f'Usage: {os.path.basename(filename)} <input file> [options]')
+    print('\nOptions:')
+    print('-o <filepath> (output filepath)')
+    print('-no (don\'t optimize output)')
+    print('-nrwf (disable subroutines for read and write)')
+    print('-cc (set number of cells)')
+    print('-cs (cell size in bits - 8, 16, 32, 64)')
+    print('-arch (architecture - i386, x86_64, arm64)')
+    print('-asm (only output assembly file, will use -o <filename> if given)')
+    print()
 
 
 def get_arg_or_default(ident: str, default: str) -> str:
-	i: int = 0
-	while i < len(sys.argv):
-		if sys.argv[i].startswith((ident, ident.upper())):
-			if len(sys.argv[i]) - len(ident) == 0:
-				if i + 1 < len(sys.argv):
-					return sys.argv[i + 1]
-				else:
-					print(f'rror: expected argument following {ident}, got none')
-			else:
-				return sys.argv[i][len(ident):]
-		i += 1
-	return default
+    i: int = 0
+    while i < len(sys.argv):
+        if sys.argv[i].startswith((ident, ident.upper())):
+            if len(sys.argv[i]) - len(ident) == 0:
+                if i + 1 < len(sys.argv):
+                    return sys.argv[i + 1]
+                else:
+                    print(
+                        f'error: expected argument following {ident}, got none'
+                    )
+            else:
+                return sys.argv[i][len(ident):]
+        i += 1
+    return default
 
 
 def get_arg_exists(ident: str) -> bool:
-	return ident in sys.argv or ident.upper() in sys.argv
+    return ident in sys.argv or ident.upper() in sys.argv
 
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		usage(__file__)
-		exit(1)
+    if len(sys.argv) < 2:
+        usage(__file__)
+        exit(1)
 
-	# TODO: use argparse instead
-	options: Options = {
-		'input_file': sys.argv[1],
-		'out': get_arg_or_default('-o', 'out'),
-		'optimize': not get_arg_exists('-no'),
-		'rwfunc': not get_arg_exists('-nrwf'),
-		'cell_count': int(get_arg_or_default('-cc', '10000')),
-		'cell_size': int(get_arg_or_default('-cs', '8')),
-		'asm_target': get_arg_or_default('-arch', 'x86_64'),
-		'size_name': '',
-		'size_prefix': '',
-		'cell_size_bytes': 1,
-		'asm_comments': True,
-		'as_args': '',
-		'ld_args': '',
-		'asm_only': get_arg_exists('-asm')
-	}
+    # TODO: use argparse instead
+    options: Options = {
+        'input_file': sys.argv[1],
+        'out': get_arg_or_default('-o', 'a.out'),
+        'optimize': not get_arg_exists('-no'),
+        'rwfunc': not get_arg_exists('-nrwf'),
+        'cell_count': int(get_arg_or_default('-c', '10000')),
+        'cell_size': int(get_arg_or_default('-s', '8')),
+        'asm_target': get_arg_or_default('-arch', 'x86_64'),
+        'size_name': '',
+        'size_prefix': '',
+        'cell_size_bytes': 1,
+        'asm_comments': True,
+        'as_args': '',
+        'ld_args': '',
+        'asm_only': get_arg_exists('-asm')
+    }
 
-	if options['asm_target'] not in ['i386', 'x86_64', 'arm64']:
-		print(f'rror: invalid arch {options["asm_target"]}')
-		exit(1)
+    if options['asm_target'] not in ['i386', 'x86_64', 'arm64']:
+        print(f'error: invalid arch {options["asm_target"]}')
+        exit(1)
 
-	# intentionally empty
-	options['as_args'] = {
+    # intentionally empty
+    options['as_args'] = {
 
-	}.get(options['asm_target'], '')
+    }.get(options['asm_target'], '')
 
-	options['ld_args'] = {
-		'i386': '-static'
-	}.get(options['asm_target'], '')
+    options['ld_args'] = {
+        'i386': '-static'
+    }.get(options['asm_target'], '')
 
-	if options['cell_size'] not in [8, 16, 32, 64]:
-		print(f'error: invalid cell size {options["cell_size"]}')
-		exit(1)
+    if options['cell_size'] not in [8, 16, 32, 64]:
+        print(f'error: invalid cell size {options["cell_size"]}')
+        exit(1)
 
-	if options['asm_target'] == 'i386' and options['cell_size'] == 64:
-		print('error: 64 bit cells not valid for i386')
-		exit(1)
+    if options['asm_target'] == 'i386' and options['cell_size'] == 64:
+        print('error: 64 bit cells not valid for i386')
+        exit(1)
 
-	options['size_name'] = {
-		8: 'byte',
-		16: 'word',
-		32: 'dword',
-		64: 'qword'
-	}.get(options['cell_size'], 'byte')
+    options['size_name'] = {
+        8: 'byte',
+        16: 'word',
+        32: 'dword',
+        64: 'qword'
+    }.get(options['cell_size'], 'byte')
 
-	options['size_prefix'] = options['size_name'][0]
+    options['size_prefix'] = options['size_name'][0]
 
-	# annoying at&t syntax thingy
-	if options['asm_target'] in ['i386', 'x86_64'] and options['cell_size'] == 32:
-		options['size_prefix'] = 'l'
-	elif options['asm_target'] in ['arm64']:
-		options['size_prefix'] = {
-			8: 'b',
-			16: 'h',
-			32: '',
-			64: ''
-		}.get(options['cell_size'], 'byte')
+    # annoying at&t syntax thingy
+    if options['asm_target'] in ['i386', 'x86_64'] and \
+            options['cell_size'] == 32:
+        options['size_prefix'] = 'l'
+    elif options['asm_target'] in ['arm64']:
+        options['size_prefix'] = {
+            8: 'b',
+            16: 'h',
+            32: '',
+            64: ''
+        }.get(options['cell_size'], 'byte')
 
-	options['cell_size_bytes'] = int(options['cell_size'] / 8)
+    options['cell_size_bytes'] = int(options['cell_size'] / 8)
 
-	compile_bf(options)
+    compile_bf(options)
--- bf_compile.py
+++ bf_compile.py
@@ -2,161 +2,182 @@ import os
 from typing import TextIO
 from types import ModuleType
 
-from bf_types import JumpPair, Program, CodeHeader, CodeFooter, Options, CodeCommand, CodeArguments
+from bf_types import JumpPair, Program, CodeHeader, \
+        CodeFooter, Options, CodeCommand, CodeArguments
 import bf_i386
 import bf_x86_64
 import bf_arm64
 
 
 def get_jump_pair(i: int, jump_pairs: list[JumpPair]) -> int:
-	for pair in jump_pairs:
-		if pair[0] == i:
-			return pair[1]
-		elif pair[1] == i:
-			return pair[0]
-	assert False, "jump pair not found."
+    for pair in jump_pairs:
+        if pair[0] == i:
+            return pair[1]
+        elif pair[1] == i:
+            return pair[0]
+    assert False, "jump pair not found."
 
 
 # TODO: stack instead of depth?
 def locate_pair(i: int, commands: Program) -> int:
-	depth: int = 0
-	for cmdi in range(i, len(commands)):
-		if commands[cmdi] == '[':
-			depth += 1
-		if commands[cmdi] == ']':
-			depth -= 1
-		if depth == 0:
-			return cmdi
-	assert False, "number of [ and ] do not match"
+    depth: int = 0
+    for cmdi in range(i, len(commands)):
+        if commands[cmdi] == '[':
+            depth += 1
+        if commands[cmdi] == ']':
+            depth -= 1
+        if depth == 0:
+            return cmdi
+    assert False, "number of [ and ] do not match"
 
 
 def gen_jump_pairs(commands: Program) -> list[JumpPair]:
-	pairs: list[JumpPair] = []
-	for i, cmd in enumerate(commands):
-		if cmd == '[':
-			pairs.append((i, locate_pair(i, commands)))
-	return pairs
+    pairs: list[JumpPair] = []
+    for i, cmd in enumerate(commands):
+        if cmd == '[':
+            pairs.append((i, locate_pair(i, commands)))
+    return pairs
 
 
 def valid_bf_char(c: str) -> bool:
-	return c in ['+', '-', '<', '>', '.', ',', '[', ']']
+    return c in ['+', '-', '<', '>', '.', ',', '[', ']']
 
 
 def write_header(f: TextIO, header: CodeHeader, options: Options) -> None:
-	for line in header(options):
-		if options['asm_comments'] or '//' not in line:
-			f.write(line)
+    for line in header(options):
+        if options['asm_comments'] or '//' not in line:
+            f.write(line)
 
 
 def write_footer(f: TextIO, footer: CodeFooter, options: Options) -> None:
-	for line in footer(options):
-		if options['asm_comments'] or '//' not in line:
-			f.write(line)
+    for line in footer(options):
+        if options['asm_comments'] or '//' not in line:
+            f.write(line)
 
 
-def write_command(f: TextIO, command: CodeCommand, instr: str, i: int, jump_pairs: list[JumpPair], options: Options) -> None:
-	instruction: str = instr[0]
-	amount: str = instr[1:]
+def write_command(f: TextIO, command: CodeCommand, instr: str, i: int,
+                  jump_pairs: list[JumpPair], options: Options) -> None:
+    instruction: str = instr[0]
+    amount: str = instr[1:]
 
-	args: CodeArguments = []
+    args: CodeArguments = []
 
-	if instruction in ['+', '-', '<', '>'] and len(amount) != 0:
-		args.append(amount)
-	elif instruction in ['[', ']']:
-		args.append(str(get_jump_pair(i, jump_pairs)))
-		args.append(str(i))
+    if instruction in ['+', '-', '<', '>'] and len(amount) != 0:
+        args.append(amount)
+    elif instruction in ['[', ']']:
+        args.append(str(get_jump_pair(i, jump_pairs)))
+        args.append(str(i))
 
-	for line in command(instruction, args, options):
-		if options['asm_comments'] or '//' not in line:
-			f.write(line)
+    for line in command(instruction, args, options):
+        if options['asm_comments'] or '//' not in line:
+            f.write(line)
 
 
 def run_optimizations(contents: str, optimize: bool) -> Program:
-	# ensure valid chars
-	contents = "".join([c for c in contents if valid_bf_char(c)])
+    # ensure valid chars
+    contents = "".join([c for c in contents if valid_bf_char(c)])
 
-	commands: Program = []
+    commands: Program = []
 
-	if optimize:
-		i = 0
-		while i < len(contents):
-			# [-] and [+] set a cell to 0
-			if i < len(contents) - 2 and contents[i] == '[' and contents[i + 1] in ('+', '-') and contents[i + 2] == ']':
-				commands.append('0')
-				i += 3
-			# if there are multiple + in a row, combine them
-			elif contents[i] == '+':
-				total = 0
-				while contents[i] == '+':
-					total += 1
-					i += 1
-				commands.append(f'+{total}')
-			# if there are multiple - in a row, combine them
-			elif contents[i] == '-':
-				total = 0
-				while contents[i] == '-':
-					total += 1
-					i += 1
-				commands.append(f'-{total}')
-			# if there are multiple < in a row, combine them
-			elif contents[i] == '<':
-				total = 0
-				while contents[i] == '<':
-					total += 1
-					i += 1
-				commands.append(f'<{total}')
-			# if there are multiple > in a row, combine them
-			elif contents[i] == '>':
-				total = 0
-				while contents[i] == '>':
-					total += 1
-					i += 1
-				commands.append(f'>{total}')
-			# otherwise just add it to the list
-			else:
-				commands.append(contents[i])
-				i += 1
-	else:
-		for c in contents:
-			if c in ['+', '-', '<', '>']:
-				commands.append(c + '1')
-			else:
-				commands.append(c)
+    if optimize:
+        i = 0
+        while i < len(contents):
+            # [-] and [+] set a cell to 0
+            if i < len(contents) - 2 and contents[i] == '[' and \
+                    contents[i + 1] in ('+', '-') and contents[i + 2] == ']':
+                commands.append('0')
+                i += 3
+            # if there are multiple + in a row, combine them
+            elif contents[i] == '+':
+                total = 0
+                while contents[i] == '+':
+                    total += 1
+                    i += 1
+                commands.append(f'+{total}')
+            # if there are multiple - in a row, combine them
+            elif contents[i] == '-':
+                total = 0
+                while contents[i] == '-':
+                    total += 1
+                    i += 1
+                commands.append(f'-{total}')
+            # if there are multiple < in a row, combine them
+            elif contents[i] == '<':
+                total = 0
+                while contents[i] == '<':
+                    total += 1
+                    i += 1
+                commands.append(f'<{total}')
+            # if there are multiple > in a row, combine them
+            elif contents[i] == '>':
+                total = 0
+                while contents[i] == '>':
+                    total += 1
+                    i += 1
+                commands.append(f'>{total}')
+            # otherwise just add it to the list
+            else:
+                commands.append(contents[i])
+                i += 1
+    else:
+        for c in contents:
+            if c in ['+', '-', '<', '>']:
+                commands.append(c + '1')
+            else:
+                commands.append(c)
 
-	return commands
+    return commands
 
 
 def compile_bf(options: Options) -> None:
 
-	contents: str = ''
-	with open(options['input_file'], 'r') as file:
-		contents = file.read()
+    contents: str = ''
+    with open(options['input_file'], 'r') as file:
+        contents = file.read()
 
-	commands: Program = run_optimizations(contents, options['optimize'])
+    commands: Program = run_optimizations(contents, options['optimize'])
 
-	options['write_used'] = commands.count('.') > 0
-	options['read_used'] = commands.count(',') > 0
+    options['write_used'] = commands.count('.') > 0
+    options['read_used'] = commands.count(',') > 0
 
-	jump_pairs: list[JumpPair] = gen_jump_pairs(commands)
+    jump_pairs: list[JumpPair] = gen_jump_pairs(commands)
 
-	asmname: str = options['out'] if options['asm_only'] else f'{options["out"]}.asm'
-	oname: str = f'{options["out"]}.o'
+    asmname: str = options['out'] if options['asm_only'] \
+        else options['out'] + '.asm'
+    oname: str = f'{options["out"]}.o'
 
-	bf_asm: ModuleType = {
-		'i386': bf_i386,
-		'x86_64': bf_x86_64,
-		'arm64': bf_arm64
-	}.get(options['asm_target'], bf_x86_64)
+    bf_asm: ModuleType = {
+        'i386': bf_i386,
+        'x86_64': bf_x86_64,
+        'arm64': bf_arm64
+    }.get(options['asm_target'], bf_x86_64)
 
-	with open(asmname, 'w') as asmfile:
-		write_header(asmfile, bf_asm.header, options)
-		for (index, cmd) in enumerate(commands):
-			write_command(asmfile, bf_asm.command, cmd, index, jump_pairs, options)
-		write_footer(asmfile, bf_asm.footer, options)
+    with open(asmname, 'w') as asmfile:
+        write_header(asmfile, bf_asm.header, options)
+        for (index, cmd) in enumerate(commands):
+            write_command(
+                asmfile,
+                bf_asm.command,
+                cmd,
+                index,
+                jump_pairs,
+                options
+            )
+        write_footer(asmfile, bf_asm.footer, options)
 
-	if not options['asm_only']:
-		os.system(f'as {asmname} -o {oname} -arch {options["asm_target"]} {options["as_args"]}')
-		os.system(f'ld {oname} -o {options["out"]} -arch {options["asm_target"]} {options["ld_args"]}')
+    if not options['asm_only']:
+        os.system(' '.join([
+            f'as {asmname}',
+            f'-o {oname}',
+            f'-arch {options["asm_target"]}'
+            f'{options["as_args"]}'
+        ]))
+        os.system(' '.join([
+            f'ld {oname}',
+            f'-o {options["out"]}',
+            f'-arch {options["asm_target"]}',
+            f'{options["ld_args"]}'
+        ]))
 
-		os.system(f'rm -f {oname}')
-		os.system(f'rm -f {asmname}')
+        os.system(f'rm -f {oname}')
+        os.system(f'rm -f {asmname}')
